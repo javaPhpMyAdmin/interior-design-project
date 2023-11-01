e8c11273e079e0f35de871dcfc67501a1f18184e
Marcelo Jose Batista Fajardo <191BATISTAMA@dnm.gub.uy>
2023-11-01 13:55:26 -0300

    tailwind.config purge changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx,css}"],
+  content: ['./src/**/*.{html,js,jsx,css}'],
   theme: {
     extend: {
       colors: {
-        primary: "rgba(255, 255, 255, 0.30)",
-        fontBlack: "#081323",
-        fontGray: "#828282",
-        overlay: "rgba(0, 0, 0, 0.30)",
+        primary: 'rgba(255, 255, 255, 0.30)',
+        fontBlack: '#081323',
+        fontGray: '#828282',
+        overlay: 'rgba(0, 0, 0, 0.30)',
       },
       screens: {
-        xs: "550px",
-        md: "850px",
-        xl: "1180px",
-        "2xl": "1280px",
+        xs: '550px',
+        md: '850px',
+        xl: '1180px',
+        '2xl': '1280px',
       },
     },
   },
   plugins: [],
+  purge: ['./src/components/**/*.{js,jsx}', './public/index.html'],
 };
